43e32649ec508b3cde24ac7e1ee646898d572b47
ayush <58028488+desibabua@users.noreply.github.com>
2020-07-03 00:17:38 +0530

    added a run function in library

--- src/library.js
+++ src/library.js
@@ -7,9 +7,13 @@ class Library {
     this.methods = {addBook : this.addBook};
   }
 
+  run(command, arguments) {
+    return this.methods[command](arguments);
+  }
+
   init(path) {
     const library = new Library(path);
-    
+
     library.db.createTable(schema1);
     library.db.createTable(schema2);
     library.db.createTable(schema3);
