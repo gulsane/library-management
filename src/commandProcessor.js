78fdfdd46c4bf582896634abff1e2224663ab4dc
ayush <58028488+desibabua@users.noreply.github.com>
2020-07-02 22:43:49 +0530

    added parser

---
+++ src/commandProcessor.js
@@ -0,0 +1,18 @@
+class Processor {
+  constructor() {
+    this.functions = {};
+  }
+
+  parse(attributes) {
+    return attributes.reduce((keyValue, attr) => {
+      const [key, value] = attr.split('=');
+      keyValue[key] = value;
+      return keyValue;
+    }, {});
+  }
+
+  process(input) {
+    const [command, ...attributes] = input.trim().split(' ');
+    return [command, this.parse(attributes)];
+  }
+}
