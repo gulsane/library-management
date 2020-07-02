38cffcb873aa74066ae2e8cb41da295bcb0cb726
ayush <58028488+desibabua@users.noreply.github.com>
2020-07-02 21:07:30 +0530

    added a command line'

--- library.js
+++ library.js
@@ -1,5 +1,14 @@
+const { stdin, stdout } = require('process');
+stdin.setEncoding('utf-8');
+stdin.setRawMode(false);
+
 const main = function () {
-  
-}
+
+  stdout.write('library $ ');
+  stdin.on('data', (data) => {
+    stdout.write(data);
+    stdout.write('library $ ');
+  });
+};
 
 main();
